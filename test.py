fffe016e8e2950194aadac2e978b2777cd631074
anakeeSakura <sakura.sirisavath@gmail.com>
2024-09-08 18:30:37 -0500

    Pushing my code

--- test.py
+++ test.py
@@ -1,1 +1,108 @@
-print("hello world")
+import math
+# some_number = ""
+# this_that = ""
+# kdkdkd= "kdasjflkdsajl"
+# iLovePizza = "peperoni"
+# # print(iLovePizza)
+
+
+# def fake_bin(x):
+#     digits = ""
+#     for num in x:
+#         # print(num)
+# #         print(num) comments are used to explain code and its color is green
+#         if int(num) < 5:
+#             digits = digits + "0"
+#             print(digits)
+            
+# #             print(digits)
+#         else:
+#             digits = digits + "1"
+#             print(digits)
+#     return digits 
+
+
+the_list = ["1", "2", "3", "4",  56]
+
+# print(the_list[0] + "10")
+# print(the_list[2] + "10")
+# # new_string = fake_bin(the_list)
+
+# print(new_string)
+
+def donuts(donuts):
+    fattyFoods = "donuts"
+# def fattyFood("donuts") 
+
+class Test:
+
+    donut =""
+    def __init__(self):
+        self.donut = "donut"
+
+    def the_method(self):
+        print("i am a method")
+
+the_class_instance = Test()
+
+print(the_class_instance.donut)
+
+
+
+theChar = 'C'
+
+print(theChar)
+
+theFloat = 2.5
+
+
+the_class_instance.the_method()
+  
+theBolean = True
+secondBolean = False
+
+earth = "round"
+try:
+    x > 3 
+except: 
+    print("pirnting somehting")
+finally:
+    print("the finally")
+
+if (earth == "round"):
+    print("The Earth Is Round") 
+elif(earth == "potato"):
+    print("the earth is a potato")
+else: 
+    print("The Earth Is Flat")
+
+
+
+
+
+count = 1
+while(theBolean):
+    print("I am in the while loop iteration: " ,     count)
+    count += 1
+    if (count == 5):
+        theBolean = False
+
+addition_numbers = 5 + 5
+print(addition_numbers)
+
+multiply_number = 10023423423400 * 2342342342323434324
+print(multiply_number)
+print(math.pi)
+
+genre = input("enter the genre: ")
+runtime = float(input("enter the runtime: "))
+if (genre == "action") and (runtime < 2):
+    print("Let's Watch It")
+elif (genre == "action") and (runtime >=2):
+    print("Can't watch no time")
+elif ( genre == "BL Movie") and (runtime < 2):
+    print("Lets Watch it!!!")
+else:
+    print("No time to Watch!!!!")
+
+    
